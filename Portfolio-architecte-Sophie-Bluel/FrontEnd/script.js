46898ed847737e0375af739d424aec4289d80612
ilhamhounzi <hounzi.ilhame@outlook.com>
2023-07-16 23:20:51 +0200

    feat: Integrate design for form page

--- Portfolio-architecte-Sophie-Bluel/FrontEnd/script.js
+++ Portfolio-architecte-Sophie-Bluel/FrontEnd/script.js
@@ -1,91 +1,144 @@
-// Set job API URL
 const worksURL = 'http://localhost:5678/api/works';
-
-// Array to store job data
 let worksData = [];
+let imageActuelle = null; 
 
-// Function that displays the cards from the gallery
-const cards = (data) => {
-
+const afficherCartes = (data) => {
   const gallery = document.querySelector('.gallery');
-
-  // Reset gallery content
   gallery.innerHTML = '';
 
-  // Iterate over job data
   data.forEach(item => {
-    // Create a <figure> element for each job
     const figure = document.createElement('figure');
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
 
-    // Add image and title to the <figure> element
-    figure.innerHTML = `<img src="${item.imageUrl}"><p>${item.title}</p>`;
+    const image = document.createElement('img');
+    image.src = item.imageUrl;
+    image.classList.add('gallery-image'); 
 
-    // Add the <figure> element to the gallery
+    imageContainer.appendChild(image);
+    figure.appendChild(imageContainer);
+
+    const title = document.createElement('p');
+    title.textContent = item.title;
+    title.classList.add('gray-title'); 
+
+    figure.appendChild(title);
     gallery.appendChild(figure);
   });
 };
 
-// Function that filters the gallery based on the selected category
-const filterGallery = (category) => {
-  let filteredWorks = [];
 
-  switch (category) {
+const filtrerGalerie = (categorie) => {
+  let travauxFiltres = [];
+
+  switch (categorie) {
     case 'all':
-      filteredWorks = worksData;
+      travauxFiltres = worksData;
       break;
     case '1':
-      filteredWorks = worksData.filter(item => item.categoryId === 1);
+      travauxFiltres = worksData.filter(item => item.categoryId === 1);
       break;
     case '2':
-      filteredWorks = worksData.filter(item => item.categoryId === 2);
+      travauxFiltres = worksData.filter(item => item.categoryId === 2);
       break;
     case '3':
-      filteredWorks = worksData.filter(item => item.categoryId === 3);
+      travauxFiltres = worksData.filter(item => item.categoryId === 3);
       break;
     default:
-      filteredWorks = worksData;
+      travauxFiltres = worksData;
   }
 
-  cards(filteredWorks);
+  afficherCartes(travauxFiltres);
 };
 
-// Select filter buttons
-const filterButtons = document.querySelectorAll('.filter-button');
-
-// Listen for clicks on the filter buttons
-filterButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Get the category of the clicked button
-    const category = button.getAttribute('data-category');
-
-    // Remove the active class from all buttons
-    filterButtons.forEach(btn => btn.classList.remove('filter-button--active'));
-
-    // Add the active class to the clicked button
-    button.classList.add('filter-button--active');
-
-    // Apply the filter based on the selected category
-    filterGallery(category);
+const boutonsFiltre = document.querySelectorAll('.filter-button');
+boutonsFiltre.forEach(bouton => {
+  bouton.addEventListener('click', () => {
+    const categorie = bouton.getAttribute('data-category');
+    boutonsFiltre.forEach(btn => btn.classList.remove('filter-button--active'));
+    bouton.classList.add('filter-button--active');
+    filtrerGalerie(categorie);
   });
 });
 
-// Fetch jobs from the API
-fetch(worksURL, {
+const afficherImagesModale = (data) => {
+  const modalGalleryContainer = document.getElementById('modalGalleryContainer');
+  modalGalleryContainer.innerHTML = '';
+
+  data.forEach((item, index) => {
+    const imageContainer = document.createElement('div');
+    imageContainer.classList.add('image-container');
+
+    const imgElement = document.createElement('img');
+    imgElement.src = item.imageUrl;
+    imgElement.classList.add('gallery-image'); 
+
+    const deleteIcon = document.createElement('i');
+    deleteIcon.classList.add('fa', 'fa-trash', 'delete-icon');
+
+
+    deleteIcon.addEventListener('click', () => {
+      supprimerImage(item.id); 
+      imageContainer.remove(); 
+      afficherCartes(worksData); 
+    });
+
+
+    imgElement.addEventListener('click', () => {
+      mettreAJourImageActuelle(item.imageUrl);
+    });
+
+    if (index === 0) {
+      const customIcon = document.createElement('i');
+      customIcon.classList.add('fa-solid', 'fa-up-down-left-right');
+      imageContainer.appendChild(customIcon);
+    }
+
+    imageContainer.appendChild(deleteIcon);
+    imageContainer.appendChild(imgElement);
+
+    const editSpan = document.createElement('span');
+    editSpan.textContent = 'éditer';
+    editSpan.classList.add('edit-span');
+
+    imageContainer.appendChild(editSpan);
+    modalGalleryContainer.appendChild(imageContainer);
+  });
+};
+
+
+const cacheBuster = Date.now(); 
+
+fetch(`${worksURL}?_=${cacheBuster}`, {
   method: 'GET',
   headers: {
-    'Accept': 'application/json'
+    'Accept': 'application/json',
+    'If-None-Match': localStorage.getItem('etag') 
   }
 })
-  .then(response => response.json())
-  .then(data => {
-    // Store job data in the worksData array
-    worksData = data;
-
-    // Display all jobs when the page loads
-    cards(worksData);
-
-    console.log(data[0]);
+  .then(response => {
+    if (response.status === 304) {
+  
+      const cachedData = JSON.parse(localStorage.getItem('worksData'));
+      worksData = cachedData;
+      afficherCartes(worksData);
+      console.log('Utilisation des données en cache');
+      return;
+    }
+    if (!response.ok) {
+      throw new Error('Erreur HTTP ' + response.status);
+    }
+    return response.json()
+      .then(data => {
+        worksData = data;
+        afficherCartes(worksData);
+        afficherImagesModale(worksData);
+        localStorage.setItem('worksData', JSON.stringify(data)); 
+        localStorage.setItem('etag', response.headers.get('etag')); 
+        console.log(data[0]);
+      });
   })
   .catch(error => {
     console.error(error);
   });
+
